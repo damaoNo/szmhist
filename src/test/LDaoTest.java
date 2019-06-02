7dad0b112f34e147d9dd70b391c6cd91bf9c1287
Elgy106 <1064959837@qq.com>
2019-06-02 16:49:29 +0800

    Merge branch 'master' of C:\GitRepository\szmhist with conflicts.

--- src/test/LDaoTest.java
+++ src/test/LDaoTest.java
@@ -1,17 +1,17 @@
 package test;
 
 import dao.ExpenseClassDao;
+import dao.IUserDao;
+import dao.UserDao;
 import vo.ExpenseClass;
 
 import java.sql.SQLException;
 
 public class LDaoTest {
     public static void main(String[] args) throws SQLException {
-//        System.out.println("ss");
-        ExpenseClassDao dao=new ExpenseClassDao();
-        ExpenseClass us=dao.readEffectiveCostSubject("XYF","");
-        System.out.println(us);
-        System.out.println("this is it");
+        IUserDao dao=new UserDao();
+        System.out.println(dao.findUserByName("hqb",""));
+
     }
 
 }
