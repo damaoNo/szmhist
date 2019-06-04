5f6bf955446c3e100b282f0d942f5070caf4b833
Elgy106 <1064959837@qq.com>
2019-06-04 15:55:36 +0800

    简单传一个医检

---
+++ src/service/medicaltreatment/PatientExaminationService.java
@@ -0,0 +1,32 @@
+package service.medicaltreatment;
+
+import dao.*;
+import vo.PatientExamination;
+import vo.Prescription;
+import vo.Register;
+
+import java.sql.SQLException;
+
+public class PatientExaminationService implements IPatientExaminationService{
+    @Override
+    public PatientExamination getPatientExamByCaseNumber(String casen) throws SQLException {
+        PatientExamination user=new PatientExamination();
+        IRegistDao dao=new RegistDao();
+        IDepartmentDao dao1=new DepartmentDao();
+        IMedicalRecordDao dao2=new MedicalRecordDao();
+        IPrescriptionDao dao3=new PrescriptionDao();
+        IUserDao dao4=new UserDao();
+
+        Register reg=dao.getRegisterByCaseNumber(casen);
+        user.setCaseNumber(reg.getCaseNumber());
+        user.setName(reg.getRealName());
+        user.setAge(reg.getAge());
+        user.setSettileId((reg.getSettLeID()==1)?"自费":"医保");
+        user.setDepartmentName(dao1.getInfByID(reg.getDeptID()).getDeptName());
+        user.setPrescriptionState(  dao3.getInfByRegistId(dao2.getInfByCaseNumber(reg.getCaseNumber()).getRegisterID()).getPrescriptionState());
+        user.setVisitDate(reg.getVisitDate().toString());
+        user.setUserName( dao4.getUserById(reg.getUserID()).getUserName());
+        user.setInvoiceNum(dao.getInfByRegistid(dao2.getInfByCaseNumber(reg.getCaseNumber()).getRegisterID()).getInvoiceNum());
+        return user;
+    }
+}
