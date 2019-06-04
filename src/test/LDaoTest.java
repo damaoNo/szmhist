300b6832cf1b557d3991084162c76ad1ab7fb708
Elgy106 <1064959837@qq.com>
2019-06-04 16:21:07 +0800

    简单传一个医检终极检测版本

--- src/test/LDaoTest.java
+++ src/test/LDaoTest.java
@@ -3,14 +3,16 @@ package test;
 import dao.ExpenseClassDao;
 import dao.IUserDao;
 import dao.UserDao;
+import service.medicaltreatment.IPatientExaminationService;
+import service.medicaltreatment.PatientExaminationService;
 import vo.ExpenseClass;
 
 import java.sql.SQLException;
 
 public class LDaoTest {
     public static void main(String[] args) throws SQLException {
-        IUserDao dao=new UserDao();
-        System.out.println(dao.findUserByName("hqb",""));
+        IPatientExaminationService ser=new PatientExaminationService();
+        System.out.println(ser.getPatientExamByCaseNumber("600606"));
 
     }
 
