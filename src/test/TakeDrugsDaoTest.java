80ba9528d9611392d8c00bc0910a79d8f20a91f2
wobuhui <fufeifeierz@qq.com>
2019-06-11 17:54:48 +0800

    0.5发药 servlet

--- src/test/TakeDrugsDaoTest.java
+++ src/test/TakeDrugsDaoTest.java
@@ -1,7 +1,9 @@
 package test;
 
+import dao.TakeDrugsDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import util.JdbcUtil;
 
 import static org.junit.Assert.*;
 
@@ -12,5 +14,11 @@ public class TakeDrugsDaoTest {
     @Test
     public void takeDrugs() {
 
+    }
+
+    @Test
+    public void changeState() {
+        TakeDrugsDao TD=new TakeDrugsDao();
+        TD.setConnection(JdbcUtil.getConnection());
     }
 }
--- src/web/medicine/TakeMedicineServlet.java
+++ src/web/medicine/TakeMedicineServlet.java
@@ -18,52 +18,64 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@WebServlet("/TakeMedicineServlet")
+@WebServlet(name="TakeMedicineServlet" ,urlPatterns ="/html/TakeMedicine/*" )
 public class TakeMedicineServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //获取请求的类型
+        /*获取请求的类型*/
         String uri=request.getRequestURI();
-        System.out.println(uri);
+        String kindM=uri.substring(uri.indexOf("TakeMedicine"));
+        String kind=kindM.substring(kindM.indexOf("/")+1);
+//        System.out.println(uri);
+//        System.out.println(kind);
+
         String  idStr=request.getParameter("binglihao");
         String  date=request.getParameter("date");
         String ID=request.getParameter("ID");
-        System.out.println(idStr);
-        System.out.println(date);
+        //System.out.println(idStr);
+        //System.out.println(date);
         System.out.println(ID);
         //查询成药处方
-        int CaseNumber=Integer.parseInt(idStr);
-        Date PrescriptionTime=null;
-        SimpleDateFormat spdf=new SimpleDateFormat("yyyy-MM-dd");
-        List list=null;
-        try {
-            PrescriptionTime=spdf.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        if ("1".equals(kind)){
+            int CaseNumber=Integer.parseInt(idStr);
+            Date PrescriptionTime=null;
+            SimpleDateFormat spdf=new SimpleDateFormat("yyyy-MM-dd");
+            List list=null;
+            try {
+                PrescriptionTime=spdf.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            ITakeDrugsService TD=new TakeDrugsService();
+            try {
+                list=TD.takeDrugs(CaseNumber,PrescriptionTime,3);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            //System.out.println(list);
+            ObjectMapper mapper=new ObjectMapper();
+            DateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+            mapper.setDateFormat(df);
+            String json=mapper.writeValueAsString(list);
+            PrintWriter pw=response.getWriter();
+            System.out.println(json);
+            pw.println(json);
+            pw.flush();
+            pw.close();
         }
-        ITakeDrugsService TD=new TakeDrugsService();
-        try {
-           list=TD.takeDrugs(CaseNumber,PrescriptionTime,3);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println(list);
-        ObjectMapper mapper=new ObjectMapper();
-        DateFormat df=new SimpleDateFormat("yyyy-MM-dd");
-        mapper.setDateFormat(df);
-        String json=mapper.writeValueAsString(list);
-        PrintWriter pw=response.getWriter();
-        System.out.println(json);
-        pw.println(json);
-        pw.flush();
-        pw.close();
+
 
         //改变药方状态
-        String[] id=ID.split(",");
-        try {
-            TD.changeState(4,id);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if ("2".equals(kind)){
+            String[] id=null;
+            id= ID.split(",");
+            ITakeDrugsService TD=new TakeDrugsService();
+            try {
+                TD.changeState(4,id);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+
 
     }
 
