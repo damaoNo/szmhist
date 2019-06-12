ff3057ef261a2980fe3ecb49ab619065dad3a4d4
Elgy106 <1064959837@qq.com>
2019-06-12 09:34:20 +0800

    医技Servlet

---
+++ src/web/doctordeal/yijiServlet.java
@@ -0,0 +1,42 @@
+package web.doctordeal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import service.medicaltreatment.IPatientExaminationService;
+import service.medicaltreatment.PatientExaminationService;
+import vo.PatientExamination;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+@WebServlet(name = "yijiServlet",urlPatterns ="/html/yiji")
+public class yijiServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("进入医技检测了哦");
+        String casn=request.getParameter("casn");
+        PatientExamination user=null;
+  //      System.out.println(casn);
+        IPatientExaminationService ser=new PatientExaminationService();
+        try {
+            user=ser.getPatientExamByCaseNumber(casn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper js=new ObjectMapper();
+        String json=js.writeValueAsString(user);
+        System.out.println(json);
+        PrintWriter pw=response.getWriter();
+        pw.println(json);
+        pw.flush();
+        pw.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
